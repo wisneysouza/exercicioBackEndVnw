58001c73663f3b9e1bb1fa9be0b1b7e0f3137911
wisneysouza <wisney.souza@gmail.com>
2025-03-30 01:05:22 -0300

    consertando lógica do exercício 6

--- 6_combinacao_dicionarios.py
+++ 6_combinacao_dicionarios.py
@@ -5,15 +5,15 @@ dicionario_2 = {'A':1, 'B':1, 'C':3, 'D':3, 'E':1}
 def combinar_dicts(dicionario_1: dict, dicionario_2: dict):
     novo_dicionario ={}
 
-    for chave_dict1, valor_dict1 in dicionario_1.items():
-
-        for chave_dict2, valor_dict2 in dicionario_2.items():
-
-            if chave_dict1 == chave_dict2:
-                novo_dicionario[chave_dict1] = valor_dict1 + valor_dict2
+    for chave, valor in dicionario_1.items():
+            if chave in dicionario_2:
+                novo_dicionario[chave] = valor + dicionario_2[chave]
             else: 
-                novo_dicionario[chave_dict1] = valor_dict1
-                novo_dicionario[chave_dict2] = valor_dict2
+                novo_dicionario[chave] = valor
+
+    for chave, valor in dicionario_2.items():
+            if chave not in novo_dicionario:
+                novo_dicionario[chave]= valor
 
     return novo_dicionario
 
